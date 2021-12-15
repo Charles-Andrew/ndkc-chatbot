9aff7d295771332d68c7ecc9b345f51752233510
Charles-Andrew <91451584+Charles-Andrew@users.noreply.github.com>
2021-12-16 01:27:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def receive_message():
         return verify_fb_token(token_sent)
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
     else:
-      start = t()
+       start = t()
         # get whatever message a user sent the bot
        output = request.get_json()
        for event in output['entry']:
