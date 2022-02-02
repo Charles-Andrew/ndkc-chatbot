183cd710a76fd28f206e1a587a2aaf3ea591abe5
Charles Andrew Barsubia <barsubiacharles@gmail.com>
2022-02-02 15:44:32 +0800

    re deploy

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ bot = Bot(ACCESS_TOKEN)
 #We will receive messages that Facebook sends our bot at this endpoint 
 @app.route("/", methods=['GET', 'POST'])
 def receive_message():
-    start = t()
     if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
         that confirms all requests that your bot receives came from Facebook.""" 
@@ -32,8 +31,7 @@ def receive_message():
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
                     response_sent_text = chatbot(message['message']['text'])
-                    end = t()
-                    send_message(recipient_id, response_sent_text,str("{:.3f}".format(end-start)))
+                    send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
                 if message['message'].get('attachments'):
                     response_sent_nontext = invalid_input()
@@ -53,9 +51,9 @@ def invalid_input():
     return random.choice(sample_responses)
 
 #uses PyMessenger to send response to user
-def send_message(recipient_id, response, ttr):
+def send_message(recipient_id, response):
     #sends user the text message provided via input response parameter
-    bot.send_text_message(recipient_id, response+" "+ttr+"s")
+    bot.send_text_message(recipient_id, response)
     return "success"
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import imp
 from train import chatbot
 
-x = input("You: ")
-y = chatbot(x)
-print(y)
+while True:
+    x = input("You: ")
+    y = chatbot(x)
+    print(y)
